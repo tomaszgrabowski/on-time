2043fa4f509ba2d7d34a058d011a6c999fb21367
Tomasz Grabowski <tomasz.grabowski@ihsmakit.com>
2020-01-13 21:44:26 +0100

    map page 2

---
+++ src/Shared/IGpsData.ts
@@ -0,0 +1,13 @@
+export interface IGpsData {
+  DataGenerated: string;
+  Line: string;
+  Route: string;
+  VehicleCode: string;
+  VehicleService: string;
+  VehicleId: number;
+  Speed: number;
+  Delay: number;
+  Lat: number;
+  Lon: number;
+  GPSQuality: number;
+}
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,27 +1,57 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GoogleMapReact from 'google-map-react';
 import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import Container from '../components/Container';
+import { IGpsData } from '../Shared/IGpsData';
 
-interface IProps {
-  vehicleId: string
+interface IMarkerProps {
+  lat: number;
+  lng: number;
+  text: string;
 }
 
+const Marker = (props: IMarkerProps) => {
+  return (
+    // <FontAwesomeIcon size='2x' icon='train' />
+    <div>{props.text}</div>
+  );
+};
+
 const MapPage = ( props: RouteComponentProps<{ vehicleId: string }> ) => {
-  
+  const vehicleId = props.match.params.vehicleId;
   const url = `http://ckan2.multimediagdansk.pl/gpsPositions`;
-  const [vehicles, setVechilces] = useState( [] );
+  const [gpsData, setGpsData] = useState<IGpsData | undefined>( undefined );
   
   useEffect( () => {
     fetch( url )
       .then( raw => raw.json()
         .then( data => {
-          const { Vehicles } = data;
-          setVechilces( Vehicles );
+          const { Vehicles }: { Vehicles: IGpsData[] } = data;
+          const x = Vehicles.find( vehicle => vehicle.VehicleId.toString() === vehicleId );
+          if ( x ) {
+            setGpsData( x );
+            console.log( x );
+          }
         } ) );
-  } );
+  }, [] );
   return (
     <Container>
-      { props.match.params.vehicleId }
+      <div style={ { height: '100vh', width: '100%' } }>
+        { gpsData && <GoogleMapReact
+          bootstrapURLKeys={ { key: 'AIzaSyADaUas3fkIvSfHUqJfR9w1vOu_eVPgEdM' } }
+          defaultCenter={ {
+            lat: gpsData.Lat,
+            lng: gpsData.Lon
+          } }
+          defaultZoom={ 16 }>
+          <Marker
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact> }
+      </div>
     </Container>
   );
 };
