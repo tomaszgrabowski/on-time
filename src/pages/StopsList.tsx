e414143be52f85a1e319dffdaaaaf1916bace024
Tomasz Grabowski <tomasz.grabowski@ihsmakit.com>
2020-02-13 14:32:35 +0100

    wip

--- src/pages/StopsList.tsx
+++ src/pages/StopsList.tsx
@@ -2,9 +2,9 @@ import * as geolib from 'geolib';
 import React, { useContext, useEffect, useState } from 'react';
 import { geolocated, GeolocatedProps } from 'react-geolocated';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { ICommonStop } from '../Shared/ICommonStop';
 import Container from '../components/Container';
 import { DataContext } from '../Shared/DataContext';
+import { ICommonStop } from '../Shared/ICommonStop';
 import { ICoords } from '../Shared/ICoords';
 import { getLocalCache } from '../Shared/LocalStorageService';
 
@@ -21,42 +21,45 @@ const StopsList = ( props: RouteComponentProps<{ city: string }> & GeolocatedPro
         if ( props.coords ) {
             const { latitude, longitude } = props.coords;
             const nearStops = data.stopData.filter( stop => isNearStop( {
-                longitude: Number(stop.stopLon),
-                latitude: Number(stop.stopLat)
+                longitude: Number( stop.stopLon ),
+                latitude: Number( stop.stopLat )
             }, { lon: longitude, lat: latitude } ) );
             setStops( nearStops );
         }
     }, [props.coords] );
     
     const isNearStop = ( point: ICoords, cords: { lon: number, lat: number } ): boolean =>
-        geolib.isPointWithinRadius(
-            point,
-            {
-                latitude: cords.lat,
-                longitude: cords.lon
-            },
-            500
-        );
+      geolib.isPointWithinRadius(
+        point,
+        {
+            latitude: cords.lat,
+            longitude: cords.lon
+        },
+        500
+      );
     return (
-        <Container>
-            <br/>
-            <br/>
-            <h3 className='text-center'>Najbliższe przystanki</h3>
-            <div className="list-group text-center">
-                { stops.length !== 0 ? stops.map( stop =>
-                        <Link to={ `/busStop/${city}/${ stop.stopId }` } key={ stop.stopId } className="list-group-item">
-                            { stop.stopFullName }
-                        </Link> )
-                    : <div className='text-center'>
-                        <div className="spinner-border text-warning" role="status">
-                            <span className="sr-only">Loading...</span>
-                        </div>
-                    </div> }
-                { stops.length !== 0 &&
-                <Link to={ `/mapPage/${city}/0/0 ` } className="list-group-item list-group-item-warning"><b>Wskaż na
-                  mapie</b></Link> }
-            </div>
-        </Container>
+      <Container>
+          <br/>
+          <br/>
+          <h3 className='text-center'>Najbliższe przystanki</h3>
+          <div className="list-group text-center">
+              { stops.length !== 0 ? stops.map( stop =>
+                  <Link to={ `/busStop/${ city }/${ stop.stopId }` } key={ stop.stopId } className="list-group-item">
+                      { stop.stopFullName }
+                  </Link> )
+                : <div className='text-center'>
+                    <div className="spinner-border text-warning" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div> }
+              { stops.length !== 0 &&
+              <Link to={ `/mapPage/${ city }/0/0 ` } className="list-group-item list-group-item-warning"><b>Wskaż na
+                mapie</b></Link> }
+          </div>
+          <Link to='/'>
+              <button className='btn btn-block btn-link'>Powrót</button>
+          </Link>
+      </Container>
     );
 };
 
